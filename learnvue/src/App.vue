0ca15651410fadc64a2d6d35ae93282fc25d7bc6
Keiter0309 <vuongtuankiet33@gmail.com>
2023-08-23 20:04:02 +0700

    initial commit

---
+++ learnvue/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="loginType === 'username'">
+      <label for="#">Username</label>
+      <input type="text">
+    </div>
+    <div v-else>
+      Time {{ hour }} : {{ minutes }} : {{ seconds }}
+    </div>
+  </div>
+  <button @click="loginType =! loginType">Click to change</button>
+  
+</template>
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      hour: new Date().getHours(),
+      minutes: new Date().getMinutes(),
+      seconds: new Date().getSeconds()
+    }
+  },
+  methods : {
+     
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
